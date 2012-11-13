7d7cbf48aab8a2e4477549290837daa27520cf9e
sean omlor <somlor@gmail.com>
2012-11-13 12:03:30 +0000

    finished exercise in lesson 10

--- game.rb
+++ game.rb
@@ -26,8 +26,29 @@ class Player
 	end
 end
 
-def hr
-	"--------------------------------"
+class Game
+	attr_reader :title
+	attr_reader :players
+
+	def initialize(title)
+		@title = title.capitalize
+		@players = [] 
+	end
+
+	def add_player(player)
+		@players.push player
+	end
+	
+	def play
+		puts "there are #{@players.size} players in the game:".capitalize
+		@players.each do |player|
+			puts player
+			player.w00t
+			player.w00t
+			player.blam
+			puts player
+		end
+	end
 end
 
 players = []
@@ -35,31 +56,14 @@ players.push Player.new("moe", 100)
 players.push Player.new("larry", 60)
 players.push Player.new("curly", 125)
 
-puts hr
-puts "there are #{players.size} players in the game:"
-
+knuckleheads = Game.new("knuckleheads")
 players.each do |player|
-	puts player
+	knuckleheads.add_player(player)
 end
+knuckleheads.play
 
-puts hr
-puts "health commissioner report:"
-players.each do |player|
-	puts player.health
-end
-
-puts hr
-puts "game begin:"
-players.each do |player|
-	player.w00t
-	player.w00t
-	player.blam
-	puts player
-end
-
-puts hr
-puts players[1].health
-players[1].name = "lawrence"
-puts players[1].name
-puts players[1].score
-puts players[1]
+chipmunks = Game.new("chipmunks")
+chipmunks.add_player(Player.new("alvin", 25))
+chipmunks.add_player(Player.new("simon", 55))
+chipmunks.add_player(Player.new("theodore", 85))
+chipmunks.play
