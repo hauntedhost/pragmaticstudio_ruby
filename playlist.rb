e0b621287f771a6ee39fbcfe300c4f227be43f6c
sean omlor <somlor@gmail.com>
2012-11-24 03:44:00 +0000

    switching if to a case in playlist.rb

--- playlist.rb
+++ playlist.rb
@@ -10,11 +10,19 @@ class Playlist
 		@movies.push movie
 	end
 
-	def play(number_rolled)
+	def roll_die
+		rand(1..6)	
+	end
+
+	def play
 		puts "#{@name}'s playlist:"
 		puts @movies
 		@movies.each do |movie|
-			if number_rolled < 5
+			number_rolled = roll_die
+			if number_rolled < 3
+				movie.thumbs_down
+				puts "#{movie.title} got a thumbs down."			
+			elsif number_rolled < 5
 				puts "#{movie.title} was skipped."
 			else
 				movie.thumbs_up
