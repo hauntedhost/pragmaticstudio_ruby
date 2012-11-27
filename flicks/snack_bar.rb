d990d528dcb2fbbb4edd687159ff7dfcf164b79d
sean omlor <somlor@gmail.com>
2012-11-27 22:35:56 +0000

    changed snack_bar to use struct, symbols and module

--- flicks/snack_bar.rb
+++ flicks/snack_bar.rb
@@ -1,13 +1,38 @@
-class Snack
-	attr_reader :name, :carbs
+#class Snack
+#	attr_reader :name, :carbs
+#
+#	def initialize(name, carbs)
+#		@name = name
+#		@carbs = carbs
+#	end
+#end
 
-	def initialize(name, carbs)
-		@name = name
-		@carbs = carbs
+Snack = Struct.new(:name, :carbs)
+
+module SnackBar
+	SNACKS = [
+		Snack.new(:popcorn, 20),
+		Snack.new(:candy, 15),
+		Snack.new(:nachos, 40),
+		Snack.new(:pretzel, 10),
+		Snack.new(:soda, 5)
+	]
+
+	def self.random
+		SNACKS.sample
 	end
 end
 
-popcorn = Snack.new("popcorn", 20)
-puts popcorn.name
-puts popcorn.carbs
+#puts SnackBar::SNACKS
+snack = SnackBar.random
+puts "enjoy your #{snack.name} (#{snack.carbs} carbs)"
+
+#popcorn = Snack.new("popcorn", 20)
+#puts popcorn.name
+#puts popcorn.carbs
+
+#candy = Snack.new("candy", 15)
+#puts candy.name
+#puts candy.carbs
+
 
