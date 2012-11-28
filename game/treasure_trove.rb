d9cfbcd4d0f740e2f535ba4049371c680dce8857
sean omlor <somlor@gmail.com>
2012-11-28 07:08:13 +0000

    updates treasures hash using ruby 1.9 key hash conventions

--- game/treasure_trove.rb
+++ game/treasure_trove.rb
@@ -3,21 +3,18 @@ Treasure = Struct.new(:name, :points)
 module TreasureTrove
 
 	treasures = {
-		:pie => 5,
-		:bottle => 25,
-		:hammer => 50,
-		:skillet => 100,
-		:broomstick => 200,
-		:crowbar => 400
+		pie: 5,
+		bottle: 25,
+		hammer: 50,
+		skillet: 100,
+		broomstick: 200,
+		crowbar: 400
 	}
-	#puts treasures
 
 	TREASURES = Array.new
 	treasures.each_pair do |name, points|
 		TREASURES.push Treasure.new(name, points)
-		#puts "#{name}, #{points}"
 	end	
-	#puts TREASURES
 
 	def self.random
 		TREASURES.sample		
