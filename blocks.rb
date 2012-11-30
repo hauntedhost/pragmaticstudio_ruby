3575f5bfcf5da0bd8d64438003df894320a74a92
sean omlor <somlor@gmail.com>
2012-11-30 09:31:04 +0000

    refresh

---
+++ blocks.rb
@@ -0,0 +1,9 @@
+3.times { puts "echo" }
+
+1.upto(10) do |number|
+	puts "#{number} situp"
+	puts "#{number} pushup"
+	puts "#{number} chinup"
+end
+
+
---
+++ sorting.rb
@@ -0,0 +1,4 @@
+names = %w(goonies ghostbusters goldfinder godfather)
+puts names.sort_by { |w| w.length }
+
+
